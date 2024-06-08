664fa0df5630effb7720882c5149f9677b404779
MANOJ R <122666116+manojgowda5@users.noreply.github.com>
2024-06-08 18:17:25 +0530

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,12 +1,2 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                    if nums[i]+nums[j]==target:
-                        a=[i,j]
-                        return a
-nums= [2,7,11,15]
-target = 9
-a=Solution()                
-a.twoSum(nums,target)
+
         
